45ae0b4fa36fe00ac8abc337924ed1b2241c52c4
222112038 <falanarofako29@gmail.com>
2024-09-10 08:50:00 +0700

    edit buletin date

--- src/models/Buletin.ts
+++ src/models/Buletin.ts
@@ -2,16 +2,16 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 interface Buletin extends Document {
     judul: string;
-    tanggal_rilis: Date;
-    tanggal_kegiatan: Date;
+    tanggal_rilis: string;
+    tanggal_kegiatan: string;
     deskripsi: string;
     link_file: string;
 }
 
 const buletinSchema: Schema = new Schema({
     judul: { type: String, required: true },
-    tanggal_rilis: { type: Date, required: true },
-    tanggal_kegiatan: { type: Date, required: true },
+    tanggal_rilis: { type: String, required: true },
+    tanggal_kegiatan: { type: String, required: true },
     deskripsi: { type: String, required: true },
     link_file: { type: String, required: true },
 });
