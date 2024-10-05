da621afc5ac4dd3317f3b65dbd5528c1f2cdea03
222112038 <falanarofako29@gmail.com>
2024-10-05 10:08:04 +0700

    update again

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,7 @@ app.use(
   })
 );
 app.use(express.json({ limit: '100mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Setup morgan to log requests
 app.use(morgan('combined'));
